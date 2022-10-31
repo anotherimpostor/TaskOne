5544c8868f8762b3790180fc11d700a02d4a7b04
anotherimpostor <phizzysmart@gmail.com>
2022-10-31 14:52:49 +0100

    taskOne

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+
+
+const app = express();
+
+
+app.use(cors({
+    origin: '*'
+}));
+
+
+app.get('/', (req, res) => {
+    res.send({
+        'SlackUsername' : '@impostor',
+        'backend' : true,
+        'age' : 21,
+        'bio'  : 'God abeg oo'
+    })
+})
+
+app.listen(process.env.PORT, () => {
+    console.log("server running");
+})
