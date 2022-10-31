5ad4fc00358995b48cf000027c2cfbc230163823
anotherimpostor <phizzysmart@gmail.com>
2022-10-31 15:28:09 +0100

    task one

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors({
 
 app.get('/', (req, res) => {
     res.send({
-        'SlackUsername' : '@impostor',
+        'slackusername' : '@impostor',
         'backend' : true,
         'age' : 21,
         'bio'  : 'God abeg oo'
